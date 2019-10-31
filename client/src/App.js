ccb6863ff16da1a18565d528473d97ccb8c53b99
Erin Davis <davisaerin@gmail.com>
2019-10-30 20:25:25 -0500

    meant to say i started mvp

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ const App = () => {
       <Route exact path="/" component={MovieList} />
       <Route path="/movies/:id" component={Movie}/>
       <div>Replace this Div with your Routes</div>
+      
     </div>
   );
 };
